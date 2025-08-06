c28f254c076a802a8d2bfec99ef73bb70b8286ba
Lewin <nickyminkels@hotmail.com>
2025-08-06 22:31:04 +0200

    Updated to nio2

--- src/main/java/nl/lewin/simpleBackups/commands/BackupCommand.java
+++ src/main/java/nl/lewin/simpleBackups/commands/BackupCommand.java
@@ -7,22 +7,25 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
+import java.util.zip.Deflater;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public final class BackupCommand implements SubCommand {
+    private static final @NotNull AtomicBoolean running = new AtomicBoolean(false);
     private final @NotNull Plugin plugin;
     private final @NotNull Logger logger;
-    private static final @NotNull AtomicBoolean running = new AtomicBoolean(false);
 
     public BackupCommand(@NotNull final Plugin plugin) {
         this.plugin = plugin;
-        logger = plugin.getLogger();
+        this.logger = plugin.getLogger();
     }
 
     @Override
@@ -35,7 +38,7 @@ public final class BackupCommand implements SubCommand {
         plugin.getServer().broadcast(Component.text("[!] Server backup has started", NamedTextColor.YELLOW));
 
         var worlds = Bukkit.getWorlds();
-        for(var world : worlds) {
+        for (var world : worlds) {
             world.save();
             world.setAutoSave(false);
             logger.info("Saved world: " + world.getName());
@@ -46,61 +49,55 @@ public final class BackupCommand implements SubCommand {
             logger.info("Saved player with UUID: " + player.getUniqueId());
         }
 
-        var backupFolder = new File(plugin.getDataFolder(), "backups");
+        var backupFolder = plugin.getDataFolder().toPath().resolve("backups");
         var fileName = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(ZonedDateTime.now()) + ".zip";
-        var backupZip = new File(backupFolder, fileName);
+        var backupZip = backupFolder.resolve(fileName);
+
         try {
-            backupFolder.mkdirs();
-            backupZip.createNewFile();
+            Files.createDirectories(backupFolder);
+            Files.createFile(backupZip);
         } catch (IOException e) {
-            logger.severe("Failed to create essential directories for backup");
+            logger.severe("Failed to create essential directories for backup: " + e.getMessage());
             return;
         }
 
         Bukkit.getScheduler().runTaskLaterAsynchronously(plugin, () -> {
-            try (var zos = new ZipOutputStream(new FileOutputStream(backupZip))) {
-                zos.setLevel(9);
+            try (var zos = new ZipOutputStream(Files.newOutputStream(backupZip, StandardOpenOption.WRITE))) {
+                zos.setLevel(Deflater.BEST_COMPRESSION);
+
                 for (var world : worlds) {
-                    var worldFolder = world.getWorldFolder();
-                    appendZip(worldFolder, world.getName(), zos);
+                    var worldFolder = world.getWorldFolder().toPath();
+                    var rootEntryName = world.getName();
+                    appendZip(worldFolder, rootEntryName, zos);
                     world.setAutoSave(true);
                 }
+            } catch (IOException e) {
+                logger.severe("Backup failed: " + e.getMessage());
+            } finally {
+                plugin.getServer().broadcast(Component.text("Server backup complete", NamedTextColor.GREEN));
+                running.set(false);
             }
-            catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            plugin.getServer().broadcast(Component.text("Server backup complete", NamedTextColor.GREEN));
         }, 20L);
     }
 
-    private void appendZip(@NotNull final File folder, @NotNull final String path, @NotNull final ZipOutputStream zos) {
-        var files = folder.listFiles();
-        if (files == null) {
-            return;
-        }
-        for (var file : files) {
-            var filePath = path + "/" + file.getName();
-            if (file.isDirectory()) {
-                appendZip(file, filePath, zos);
-            }
-            else {
-                // Skip "session.lock" because it can cause issues
-                if(file.getName().equals("session.lock")) {
-                    continue;
+    private void appendZip(@NotNull final Path folder, @NotNull final String rootPath, @NotNull final ZipOutputStream zos) throws IOException {
+        Files.walkFileTree(folder, new SimpleFileVisitor<>() {
+            @Override
+            public @NotNull FileVisitResult visitFile(@NotNull final Path file, @NotNull final BasicFileAttributes attrs) throws IOException {
+                if (file.getFileName().toString().equals("session.lock")) {
+                    return FileVisitResult.CONTINUE;
                 }
-                try(var bis = new BufferedInputStream(new FileInputStream(file))) {
-                    zos.putNextEntry(new ZipEntry(filePath));
-                    var buffer = new byte[16384];
-                    int length;
-                    while ((length = bis.read(buffer)) != -1) {
-                        zos.write(buffer, 0, length);
-                    }
-                    zos.closeEntry();
-                } catch (IOException e) {
-                    logger.severe("Failed to append to zip: " + path);
-                }
+
+                var relativePath = folder.relativize(file);
+                var zipEntryName = rootPath + "/" + relativePath.toString().replace("\\", "/");
+
+                zos.putNextEntry(new ZipEntry(zipEntryName));
+                Files.copy(file, zos);
+                zos.closeEntry();
+
+                return FileVisitResult.CONTINUE;
             }
-        }
+        });
     }
 
     @Override
